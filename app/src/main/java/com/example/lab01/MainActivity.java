2041811154a3a600cb2b11f3f3f2f04c7e668af5
Moooooona <najafzadehmona977@gmail.com>
2022-10-06 15:43:12 +0300

    added change text button

--- app/src/main/java/com/example/lab01/MainActivity.java
+++ app/src/main/java/com/example/lab01/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lab01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.btn1);
+        tv1 = (TextView) findViewById(R.id.tv);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View argo) {
+                tv1.setText("Welcome Dear!");
+            }
+        });
     }
 }
