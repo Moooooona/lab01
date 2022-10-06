3fd8f49949aa20860bc373b1e9c01587d7050626
Moooooona <najafzadehmona977@gmail.com>
2022-10-06 15:49:21 +0300

    added change color text button

--- app/src/main/java/com/example/lab01/MainActivity.java
+++ app/src/main/java/com/example/lab01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab01;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    Button b1, b2 ;
     TextView tv1;
 
     @Override
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         b1 = (Button) findViewById(R.id.btn1);
+        b2=(Button) findViewById(R.id.btn2);
         tv1 = (TextView) findViewById(R.id.tv);
 
         b1.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 tv1.setText("Welcome Dear!");
             }
         });
+        b2.setOnClickListener(new View.OnClickListener(){
+                                  @Override
+                                  public void onClick(View argo){
+                                      tv1.setTextColor(Color.RED);
+                                  }
+        });
+
     }
 }
